2d91e3edbd24167af6fa09cc875d8067f50eac50
Jean Carlos Nesi <jean.carlos_nesi@hotmail.com>
2022-05-26 10:36:09 -0300

    Delete .slicemachine directory

--- .slicemachine/assets/slices/1/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/1';
-
-export default {
-  title: 'slices/1'
-}
-
-
-export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"1","items":[],"primary":{"title":[{"type":"heading1","text":"Whiteboard vertical content","spans":[]}],"description":[{"type":"paragraph","text":"Excepteur minim commodo non aliqua in id id ea sunt reprehenderit id.","spans":[]}]},"id":"_Default"}} />
-_Default.storyName = 'Default'
